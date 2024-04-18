9fb3371d0fba2e6305ad6110780cebd944dd9a27
Jaiden Gool <349162586@gapps.yrdsb.ca>
2024-04-18 13:33:46 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,20 +1,23 @@
 public class Student
 {
   public String firstName;
+  public int gradeLevel;
 
-  public Student(String firstName)
+  public Student(String firstName, int gradeLevel)
   {
     this.firstName = firstName;
+    this.gradeLevel = gradeLevel;
   }
 
   public void printName()
   {
     System.out.println("Student name: " + firstName);
+    System.out.println("Student grade: " + gradeLevel);
   }
   
   public static void main(String[] args)
   {
-    Student jaiden = new Student("Jaiden");
+    Student jaiden = new Student("Jaiden", 11);
     jaiden.printName();
   }
 }
